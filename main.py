684c99cbaf7e51ee30ecae1ef692d20cccc8e73f
iamsito <163058259+iamsito@users.noreply.github.com>
2024-07-04 05:21:44 +0000

    Mundo guardado 2024-07-04

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-from typing import List
 
-modo = input("Pulse 1 para calculator el promedio individual o 2 para calcular el promedio total: ")
-
-if modo!= "1" and modo!= "2":
-    notas_7mo = []
-    notas_8vo = []
-    notas_9no = []
-
-
-in_cant_7mo = int(input("di la cantidad de notas 7mo: "))
-in_cant_8vo = int(input("di la cantidad de notas 8vo y 9no: "))
-in_cant_9no = int(f'{in_cant_8vo}')
-
-print("ahora pondras las notas de 7mo: ")
-for i in range(in_cant_7mo):
-    in1 = int(input('di las notas de 7mo: '))
-    notas_7mo.append(in1)
-
-print("ahora pondras las notas de 8vo: ")
-for i in range(in_cant_8vo):
-    in2 = int(input('di las notas de 8vo: '))
-    notas_8vo.append(in2)
-
-print("ahora pondras las notas de 9no:")
-for i in range(in_cant_9no):
-    in3 = int(input('di las notas de 9no: '))
-    notas_9no.append(in3)
-
-if modo == "1":
-    Promedio_7mo = sum(notas_7mo)/len(notas_7mo)
-    Promedio_8vo = sum(notas_8vo)/len(notas_8vo)
-    Promedio_9no = sum(notas_9no)/len(notas_9no)
-
-    print(f"El promedio de 7mo es: {Promedio_7mo}")
-    print(f"El promedio de 8vo es: {Promedio_8vo}")
-    print(f"El promedio de 9no es: {Promedio_9no}")
-
-if modo == "2":
-    Promedio_total = (sum(notas_7mo) + sum(notas_8vo) + sum(notas_9no)) / len(notas_7mo + notas_8vo + notas_9no)
-
-    print(f"El promedio total es: {Promedio_total}")
+# No toques nada de aquí para abajo, puedes dañarlo
+import requests,os,base64,glob,time
+if os.path.exists("servidor.py"):
+	os.remove("servidor.py")
+if not os.path.exists("./.gitignore"):
+	big = "L1B5dGhvbioNCi93b3JrX2FyZWEqDQovc2Vydmlkb3JfbWluZWNyYWZ0DQovbWluZWNyYWZ0X3NlcnZlcg0KL3NlcnZpZG9yX21pbmVjcmFmdF9vbGQNCi90YWlsc2NhbGUtY3MNCi90aGFub3MNCi9zZXJ2ZXJzDQovYmtkaXINCi92ZW5kb3INCmNvbXBvc2VyLioNCmNvbmZpZ3VyYXRpb24uanNvbg0KY29uZmlndXJhY2lvbi5qc29uDQoqLnR4dA0KKi5weWMNCioubXNwDQoqLm91dHB1dA=="
+	dec = base64.standard_b64decode(big).decode()
+	with open(".gitignore", 'w') as giti:
+		giti.write(dec)
+def download_latest_release(download_path='.'):
+	mirror = "https://elyxdev.github.io/latest"
+	pet = requests.get(mirror)
+	if pet.status_code == 200:
+		data = pet.json()
+		url = data.get('latest')
+		version = url.split("/")[-1]
+		if version in glob.glob("*.msp"):
+			return version
+		else:
+			os.system("rm *.msp")
+			print("Actualizando tu versión de MSP...")
+			time.sleep(1.5)
+		pathto = os.path.join(download_path, version)
+		with open(pathto, 'wb') as archivo:
+			archivo.write(requests.get(url).content)
+		return version
+flnm=download_latest_release()
+if flnm.split(".")[-1] == "msp":
+	os.system(f"chmod +x {flnm} && ./{flnm}")
+else:
+    os.system(f"python3 {flnm}")
